3f8fdbee134e276e9f99301983551d10477352a8
Ivan Tikhonov <kefeer@brokestream.com>
2011-03-02 19:12:37 +0500

    tag wiring

--- ec.c
+++ ec.c
@@ -139,7 +139,7 @@ void Pt8(cairo_t *c, int x, int y) {
 	cairo_rectangle(c,x+100,y+100,700,700);
 	cairo_stroke(c);
 
-	cairo_set_source_rgb(c,1,0,0);
+	cairo_set_source_rgba(c,0,0,0,0.5);
 	int i;
 
 	for(i=0;i<8;i++) { cairo_rectangle(c,x,y+80*i+140,100,37); cairo_fill(c); }
@@ -157,8 +157,8 @@ void Pt8(cairo_t *c, int x, int y) {
 struct tag {
 	int x,y; // 0.01mm units
 	void (*p)(cairo_t *,int,int);
-} tag[1024]={{1000,1000,Pt8}};
-int tagn=1;
+} tag[1024]={{1000,1000,Pt8},{2000,1500,Pt8}};
+int tagn=2;
 
 void addtag(int x,int y) {
 	tag[tagn].x=x;
@@ -168,8 +168,24 @@ void addtag(int x,int y) {
 }
 
 
+
 /*
- * o66/72 - put new tag at 66x72
+ * Data: wires
+ */
+
+struct wire {
+	struct { int tag; int pin; } a,b;
+} wire[1024]={{{0,0},{1,0}}};
+int wiren=1;
+
+void addwire(int a, int b) {
+	wire[wiren].a.tag=a;
+	wire[wiren].b.tag=b;
+	wiren++;
+}
+
+/*
+ * o662 - put new tag at 6:6:2
  * 67pt8 - assign tag #67 to be in a package TQFP-32 (7x7mm lead: 1mm length 0.8mm space 0.37mm width)
  * w67/8-o8745-69/2-72 - wire from pin 8 of #67 to 87x45 to pin 2 of 69 to point 72
  * u67/8-o8745-69/2-72 - unwire from pin 8 of #67 to 87x45 to pin 2 of 69 to point 72
@@ -234,6 +250,56 @@ void target_grid(int x, int y, int sx, int sy) {
 }
 
 void draw_tag(cairo_t *c, char *id, struct tag *t);
+void draw_wire(cairo_t *c, struct wire *w);
+
+void Sidle(char);
+
+void Swire(char c) {
+	cairo_move_to(overlay,100,100);
+	cairo_show_text(overlay,"WIRE");
+
+	static int ts=0, ps=-1;
+
+	char buf[32];
+	switch(c) {
+	case '0': case '1': case '2': case '3': case '4': case '5': case '6': case '7': case '8': case '9':
+		ts*=10; ts+=c-'0';
+		if(ts<tagn) {
+			cairo_save(overlay);
+			cairo_set_source_rgba(overlay,1,0,0,0.8);
+			cairo_rectangle(overlay,tag[ts].x,tag[ts].y,100,100);
+			cairo_fill(overlay);
+
+			if(ps>=0) {
+				struct wire w={{ps,0},{ts,0}};
+				draw_wire(overlay,&w);
+				snprintf(buf,sizeof(buf),"%u - %u",ps,ts);
+			} else {
+				snprintf(buf,sizeof(buf),"%u",ts);
+			}
+			cairo_move_to(overlay,500,100);
+			cairo_show_text(overlay,buf);
+
+			cairo_restore(overlay);
+		}
+		break;
+	case '-':
+		if(ps>=0) addwire(ps,ts);
+		ps=ts; ts=0;
+		snprintf(buf,sizeof(buf),"%u - ",ps);
+		cairo_move_to(overlay,500,100);
+		cairo_show_text(overlay,buf);
+		break;
+	case '\r':
+		if(ps>=0) addwire(ps,ts);
+	case 27:
+		Sidle(0); break;
+	case 0:
+		ts=0; ps=-1;
+		state=WIRE;
+	}
+	
+}
 
 void Stag(char c) {
 	cairo_move_to(overlay,100,100);
@@ -256,6 +322,7 @@ void Stag(char c) {
 	case '7': dx=0; dy=2; goto update_grid;
 	case '8': dx=1; dy=2; goto update_grid;
 	case '9': dx=2; dy=2; goto update_grid;
+
 	update_grid:
 		grid_step(step,&sx,&sy);
 		x+=sx*dx;
@@ -270,24 +337,25 @@ void Stag(char c) {
 	case '\r':
 		addtag(x,y);
 	case 27:
-		state=IDLE; break;
+		Sidle(0); break;
 	case 0:
+		state=TAG;
 		x=y=step=0;
 		grid_step(step,&sx,&sy);
 		target_grid(x,y,sx,sy);
 	}
-
-	return;
 }
 
 
-void Sidle(c) {
+void Sidle(char c) {
 	switch(c) {
-	case 'o': state=TAG; Stag(0); break;
-	case 'w': state=WIRE; break;
+	case 'o': Stag(0); break;
+	case 'w': Swire(0); break;
 	case 'u': state=UNWIRE; break;
 	case '0': case '1': case '2': case '3': case '4': case '5': case '6': case '7': case '8': case '9':
 		state=EDIT; break;
+	case 0:
+		state=IDLE;
 	}
 }
 
@@ -305,7 +373,7 @@ int handle_key(char c) {
 		Sidle(c); break;
 	case TAG:	Stag(c); break;
 	case EDIT:	//Sedit(c); break;
-	case WIRE:	//Swire(c); break;
+	case WIRE:	Swire(c); break;
 	case UNWIRE:	//Sunwire(c); break;
 		break;
 	}
@@ -325,6 +393,17 @@ void draw_tag(cairo_t *c, char *id, struct tag *t) {
 	}
 }
 
+void draw_wire(cairo_t *c, struct wire *w) {
+	struct tag *a=tag+w->a.tag;
+	struct tag *b=tag+w->b.tag;
+	cairo_save(c);
+	cairo_set_line_width(c,30);
+	cairo_move_to(c,a->x,a->y);
+	cairo_line_to(c,b->x,b->y);
+	cairo_stroke(c);
+	cairo_restore(c);
+}
+
 void draw(cairo_t *c) {
 	int i;
 	for(i=0;i<tagn;i++) {
@@ -332,6 +411,10 @@ void draw(cairo_t *c) {
 		char buf[256];
 		snprintf(buf,sizeof(buf),"%u",i);
 		draw_tag(c,buf,t);
+	}
+
+	for(i=0;i<wiren;i++) {
+		draw_wire(c,wire+i);
 	}
 }
 
